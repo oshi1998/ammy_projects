94296ff4f677eec81c454e2145fe972f9325b5fb
oshi1998 <76892994+oshi1998@users.noreply.github.com>
2021-10-12 12:54:40 +0700

    v1.0.7

--- WEB_SYSTEM/services/db_config.php
+++ WEB_SYSTEM/services/db_config.php
@@ -11,7 +11,7 @@ $password = "";
 $db = "ammy_projects";
 
 $pdo;
-$dsn = "mysql:host=$host;dbname=$db";
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8";
 $pdo = new PDO($dsn, $username, $password);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
